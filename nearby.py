9eda3595a56c0db4a0218ef02da0c3bd338dee00
Oscar <oscar.nesis@gmail.com>
2016-08-28 14:34:43 +0200

    Use of env variables instead of config.json file

---
+++ nearby.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+"""
+pgoapi - Pokemon Go APItest
+Copyright (c) 2016 tjado <https://github.com/tejado>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+OR OTHER DEALINGS IN THE SOFTWARE.
+
+Author: tjado <https://github.com/tejado>
+"""
+
+import os
+import sys
+import json
+import time
+import pprint
+import logging
+import getpass
+import argparse
+
+# add directory of this file to PATH, so that the package will be found
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+# import Pokemon Go API lib
+from pgoapi import pgoapi
+from pgoapi import utilities as util
+
+
+log = logging.getLogger(__name__)
+
+def init_config():
+
+    config.auth_service = os.environ['AUTH_SERVICE']
+    config.username = os.environ['USERNAME']
+    config.password = os.environ['PASSWORD']
+    config.location = os.environ['LOCATION']
+    config.debug = os.environ['DEBUG']
+    config.test = os.environ['TEST']
+
+    return config
+
+
+def main():
+    # log settings
+    # log format
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(module)10s] [%(levelname)5s] %(message)s')
+    # log level for http request class
+    logging.getLogger("requests").setLevel(logging.WARNING)
+    # log level for main pgoapi class
+    logging.getLogger("pgoapi").setLevel(logging.INFO)
+    # log level for internal pgoapi class
+    logging.getLogger("rpc_api").setLevel(logging.INFO)
+
+    config = init_config()
+    if not config:
+        return
+
+    if config.debug:
+        logging.getLogger("requests").setLevel(logging.DEBUG)
+        logging.getLogger("pgoapi").setLevel(logging.DEBUG)
+        logging.getLogger("rpc_api").setLevel(logging.DEBUG)
+
+
+    # instantiate pgoapi
+    api = pgoapi.PGoApi()
+
+    # parse position
+    position = util.get_pos_by_name(config.location)
+    if not position:
+        log.error('Your given location could not be found by name')
+        return
+    elif config.test:
+        return
+
+    # set player position on the earth
+    api.set_position(*position)
+
+    if not api.login(config.auth_service, config.username, config.password, app_simulation = True):
+        return
+
+    # get player profile call (single command example)
+    # ----------------------
+    response_dict = api.get_player()
+    print('Response dictionary (get_player): \n\r{}'.format(pprint.PrettyPrinter(indent=4).pformat(response_dict)))
+
+    # sleep due to server-side throttling
+    time.sleep(0.2)
+
+    # get player profile + inventory call (thread-safe/chaining example)
+    # ----------------------
+    req = api.create_request()
+    req.get_player()
+    req.get_inventory()
+    response_dict = req.call()
+    print('Response dictionary (get_player + get_inventory): \n\r{}'.format(pprint.PrettyPrinter(indent=4).pformat(response_dict)))
+
+if __name__ == '__main__':
+    main()
